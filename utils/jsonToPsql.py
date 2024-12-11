5d791e93ffdd29cce8a9df3435e63f2a3678c3e0
Torch <rustruedetective@gmail.com>
2024-12-11 21:41:33 +0500

    added foreign keys

--- utils/jsonToPsql.py
+++ utils/jsonToPsql.py
@@ -5,9 +5,29 @@ output_path_db = "../design/queries/postgresql/query_create_db.sql"
 output_path_schema = "../design/queries/postgresql/query_create_schema.sql"
 output_path_table = "../design/queries/postgresql/query_create_table.sql"
 
-def addForeignKeys(cols):
-    # print(cols)
-    return cols
+def processCols(schema, table, col):
+    """
+        Description:
+            1. Adds the query : "ColumnName" DATATYPE CONSTRAINT
+               to each column of table
+            2. Checks if a foreign key is needed or not.
+               The check is made using the "Id" keyword in the column name.
+
+        Args:
+            schema: <str>
+                Name of schema to put the table and its columns into
+            table: <str>
+                Name of table
+            col: <str>
+                Name of column to
+                1. put into format "ColumnName" DATATYPE CONSTRAINT
+                2. check if needs foreign key constraints
+    """
+
+    if (col.split(" ")[-1] == "Id") and (len(col.split(" ")) > 1):
+        ref_table = " ".join(col.split(" ")[:-1]).strip()
+        return f"\t\"{col}\" {content[schema][table][col]}, FOREIGN KEY (\"{col}\") REFERENCES \"{ref_table}\"(Id),"
+    return f"\t\"{col}\" {content[schema][table][col]},"
 
 with open(input_path) as file:
     content = file.read()
@@ -30,7 +50,6 @@ with open(output_path_table, "w") as file:
     query = ""
     for schema in content.keys():
         for table in content[schema].keys():
-            cols = [f"\t\"{col}\" {content[schema][table][col]}," for col in content[schema][table]]
-            cols = addForeignKeys(cols)
+            cols = [processCols(schema, table, col) for col in content[schema][table]]
             query = query + create_table.format(schema, table, "\n".join(cols))
     file.write(query)
